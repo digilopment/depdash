7874d8a881026341c2fb0ace80b3379bc99e2ac6
Tomáš Doubek <doubek.tomas@markiza.sk>
2023-04-29 12:35:31 +0000

    autoload full path for bin

--- public/bin.php
+++ public/bin.php
@@ -1,5 +1,5 @@
 <?php
 
-file_exists('../src/Config.php') ? require_once '../src/Config.php' : require_once '../src/ConfigDefault.php';
-require_once '../src/DepDash.php';
+file_exists(__DIR__ . '/../src/Config.php') ? require_once __DIR__ . '/../src/Config.php' : require_once __DIR__ . '/../src/ConfigDefault.php';
+require_once __DIR__ . '/../src/DepDash.php';
 (new DepDash())->getResponse()->withJson()->writeToFile();
